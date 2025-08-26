5c898e3a1b85cdfae744b351bbbee5d94f1e2907
sinyonar0623 <chinnaphon.trw@gmail.com>
2025-08-26 16:03:11 +0700

    Fix Some Issue

--- Modules/Collateral/Collateral/CollateralVessels/Models/CollateralVessel.cs
+++ Modules/Collateral/Collateral/CollateralVessels/Models/CollateralVessel.cs
@@ -3,7 +3,6 @@ namespace Collateral.CollateralVessels.Models;
 public class CollateralVessel : Aggregate<long>
 {
     public long CollatId { get; private set; } = default!;
-    public long ApprId { get; private set; } = default!;
     public CollateralProperty CollateralVesselProperty { get; private set; } = default!;
     public CollateralDetail CollateralVesselDetail { get; private set; } = default!;
     public CollateralSize CollateralVesselSize { get; private set; } = default!;
@@ -12,21 +11,18 @@ public class CollateralVessel : Aggregate<long>
 
     private CollateralVessel(
         long collatId,
-        long apprId,
         CollateralProperty collateralVesselProperty,
         CollateralDetail collateralVesselDetail,
         CollateralSize collateralVesselSize
     )
     {
         CollatId = collatId;
-        ApprId = apprId;
         CollateralVesselProperty = collateralVesselProperty;
         CollateralVesselDetail = collateralVesselDetail;
         CollateralVesselSize = collateralVesselSize;
     }
     public static CollateralVessel Create(
         long collatId,
-        long apprId,
         CollateralProperty collateralVesselProperty,
         CollateralDetail collateralVesselDetail,
         CollateralSize collateralVesselSize
@@ -34,7 +30,6 @@ public class CollateralVessel : Aggregate<long>
     {
         return new CollateralVessel(
             collatId,
-            apprId,
             collateralVesselProperty,
             collateralVesselDetail,
             collateralVesselSize
