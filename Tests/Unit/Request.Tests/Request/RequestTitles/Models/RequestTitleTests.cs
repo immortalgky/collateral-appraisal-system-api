b5d412e3d0163659619cf1ae615024b75fc33aca
d03n <nattar.1506@gmail.com>
2025-11-23 22:52:00 +0700

    update test

--- Tests/Unit/Request.Tests/Request/RequestTitles/Models/RequestTitleTests.cs
+++ Tests/Unit/Request.Tests/Request/RequestTitles/Models/RequestTitleTests.cs
@@ -16,17 +16,17 @@ public class RequestTitleDataBuilder
   public Guid _requestId = Guid.NewGuid();
   public string _collateralType = "Land";
   public bool _collateralStatus = true;
-  public TitleDeedInfo _titleDeedInfo = new("123", "Chanote", "detail");
-  public SurveyInfo _surveyInfo = new("rawang", "landNo", "surveyNo");
-  public LandArea _landArea = new(1, 2, 3);
+  public TitleDeedInfo _titleDeedInfo = TitleDeedInfo.Create("123", "Chanote", "detail");
+  public SurveyInfo _surveyInfo = SurveyInfo.Create("rawang", "landNo", "surveyNo");
+  public LandArea _landArea = LandArea.Of(1, 2, 3);
   public string _ownerName = "Owner";
   public string _registrationNo = "REG123";
-  public VehicleInfo _vehicleInfo = new("Car", "Location", "CHASIS");
-  public MachineInfo _machineInfo = new("Status", "Type", "Installed", "INV123", 1);
-  public BuildingInfo _buildingInfo = new("Type", 100, 1);
-  public CondoInfo _condoInfo = new("CondoName", "B1", "101", "10");
-  public Address _titleAddress = new("house", "proj", "moo", "soi", "road", "sub", "dist", "prov", "50000");
-  public Address _dopaAddress = new("house", "proj", "moo", "soi", "road", "sub", "dist", "prov", "50000");
+  public VehicleInfo _vehicleInfo = VehicleInfo.Create("Car", "Location", "CHASIS");
+  public MachineInfo _machineInfo = MachineInfo.Create("Status", "Type", "Installed", "INV123", 1);
+  public BuildingInfo _buildingInfo = BuildingInfo.Create("Type", 100, 1);
+  public CondoInfo _condoInfo = CondoInfo.Create("CondoName", "B1", "101", "10");
+  public Address _titleAddress = Address.Create("house", "room", "floorNo", "proj", "moo", "soi", "road", "sub", "dist", "prov", "50000");
+  public Address _dopaAddress = Address.Create("house", "room", "floorNo","proj", "moo", "soi", "road", "sub", "dist", "prov", "50000");
   public string _notes = "notes";
 
   public RequestTitleData Build() => new()
