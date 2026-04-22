f45b0aa0f7d554f2357216ebb12ee7f97bfb15b4
thunnuht <thunyathep2544@gmail.com>
2026-04-22 17:05:36 +0700

    change field length title number in land detail

---
+++ Modules/Appraisal/Appraisal/Infrastructure/Migrations/20260422100113_ChangeFieldLengthTitleNumber.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Appraisal.infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeFieldLengthTitleNumber : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "TitleNumber",
+                schema: "appraisal",
+                table: "LandTitles",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "TitleNumber",
+                schema: "appraisal",
+                table: "LandTitles",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200);
+        }
+    }
+}
