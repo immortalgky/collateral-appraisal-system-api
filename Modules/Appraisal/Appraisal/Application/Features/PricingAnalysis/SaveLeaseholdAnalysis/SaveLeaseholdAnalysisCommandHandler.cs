e032f04ee0a25f1587bf0eebe3a79993bcd70281
d03 <nattar.1506@gmail.com>
2026-05-05 14:40:50 +0700

    Update SaveLeaseholdAnalysisCommandHandler.cs

--- Modules/Appraisal/Appraisal/Application/Features/PricingAnalysis/SaveLeaseholdAnalysis/SaveLeaseholdAnalysisCommandHandler.cs
+++ Modules/Appraisal/Appraisal/Application/Features/PricingAnalysis/SaveLeaseholdAnalysis/SaveLeaseholdAnalysisCommandHandler.cs
@@ -40,6 +40,18 @@ public class SaveLeaseholdAnalysisCommandHandler(
             analysis = LeaseholdAnalysis.Create(method.Id);
             method.SetLeaseholdAnalysis(analysis);
         }
+        
+        // Update partial usage (lease land area) to calculate leashold table first
+        if (command.IsPartialUsage)
+        {
+            analysis.SetPartialUsage(true,
+                  command.PartialRai, command.PartialNgan, command.PartialWa,
+                  (command.PartialRai ?? 0) * 400m + (command.PartialNgan ?? 0) * 100m + (command.PartialWa ?? 0), command.PricePerSqWa,
+                  null, null, null
+                  );
+        } else {
+            analysis.SetPartialUsage(false, null, null, null, null, null, null, null, null );
+        }
 
         // Update input fields
         analysis.Update(
