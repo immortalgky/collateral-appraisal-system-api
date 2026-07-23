0da3eb2728bf33f67785b6bb6d9d5406b4257f3a
thunnuht <thunyathep2544@gmail.com>
2026-07-23 12:36:54 +0700

    Add column UseSystemCalc in method

---
+++ Modules/Appraisal/Appraisal/Infrastructure/Migrations/20260723053420_AddUseSystemCalc.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Appraisal.infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUseSystemCalc : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "UseSystemCalc",
+                schema: "appraisal",
+                table: "PricingAnalysisMethods",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UseSystemCalc",
+                schema: "appraisal",
+                table: "PricingAnalysisMethods");
+        }
+    }
+}
