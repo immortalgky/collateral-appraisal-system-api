80fd256c25efd95b509276fe90bfca5d20abf248
thunnuht <thunyathep2544@gmail.com>
2026-07-23 11:54:00 +0700

    change field length decision screen

---
+++ Modules/Appraisal/Appraisal/Infrastructure/Migrations/20260723044745_ChangeFieldLengthOpinionDecision.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Appraisal.infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeFieldLengthOpinionDecision : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UseSystemCalc",
+                schema: "appraisal",
+                table: "PricingAnalysisMethods");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "InternalAppraiserOpinion",
+                schema: "appraisal",
+                table: "AppraisalDecisions",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(4000)",
+                oldMaxLength: 4000,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ExternalAppraiserOpinion",
+                schema: "appraisal",
+                table: "AppraisalDecisions",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(4000)",
+                oldMaxLength: 4000,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "UseSystemCalc",
+                schema: "appraisal",
+                table: "PricingAnalysisMethods",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "InternalAppraiserOpinion",
+                schema: "appraisal",
+                table: "AppraisalDecisions",
+                type: "nvarchar(4000)",
+                maxLength: 4000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ExternalAppraiserOpinion",
+                schema: "appraisal",
+                table: "AppraisalDecisions",
+                type: "nvarchar(4000)",
+                maxLength: 4000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
